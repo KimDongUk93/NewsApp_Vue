fdb4abb844c1cce41760bdcfa901344997946263
KimDonguk/FE <ehddnr3540@gmail.com>
2022-03-24 00:24:37 +0900

    Add routes

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/news">news</router-link>
+      </li>
+      <li>
+        <router-link to="/ask">ask</router-link>
+      </li>
+      <li>
+        <router-link to="/jobs">jobs</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
